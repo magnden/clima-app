d6ab9996a50c73a68a51a44fe01cc1d47eabd81a
Dennis Magnusson <dennis@Dennis-MacBook-Pro.local>
2017-09-25 15:42:16 +0300

    added city search

--- Clima/WeatherDataModel.swift
+++ Clima/WeatherDataModel.swift
@@ -14,7 +14,10 @@ class WeatherDataModel {
     var temperature: Int = 0
     var condition: Int = 0
     var city: String = ""
+    var country: String = ""
     var weatherIconName: String = ""
+    var weatherBackgroundName: String = ""
+    var weatherCondition: String = ""
     
     //This method turns a condition code into the name of the weather condition image
     
@@ -59,5 +62,67 @@ class WeatherDataModel {
             return "dunno"
         }
 
+    }
+    
+    func updateWeatherBackground(condition: Int) -> String {
+        
+        switch (condition) {
+            
+        case 0...300, 772...799, 900...903, 905...1000 :
+            return "backgroundThunder"
+            
+        case 301...500, 501...600 :
+            return "backgroundRain"
+            
+        case 601...700, 903 :
+            return "backgroundSnow"
+            
+        case 701...771 :
+            return "backgroundFog"
+
+        case 800, 904 :
+            return "backgroundSun"
+            
+        case 801...804 :
+            return "backgroundCloud"
+            
+        default :
+            return "background"
+        }
+        
+    }
+    
+    func updateWeatherCondition(condition: Int) -> String {
+        
+        switch (condition) {
+            
+        case 0...300, 772...799, 900...903, 905...1000 :
+            return "Thunder Storms"
+            
+        case 301...500 :
+            return "Light Rain"
+            
+        case 501...600 :
+            return "Showers"
+            
+        case 601...700, 903 :
+            return "Snow"
+            
+        case 701...771 :
+            return "Fog"
+            
+        case 800 :
+            return "Sunny"
+            
+        case 801...804 :
+            return "Cloudy"
+
+        case 904 :
+            return "Sunny"
+            
+        default :
+            return "Conditions not available"
+        }
+        
     }
 }
--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -12,7 +12,7 @@ import Alamofire
 import SwiftyJSON
 
 
-class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -28,7 +28,9 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
+    @IBOutlet weak var weatherBackground: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,7 +58,6 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             response in
             if response.result.isSuccess {
                 print("Success! Got the weather data")
-                
                 let weatherJSON: JSON = JSON(response.result.value!)
                 self.updateWeatherData(json: weatherJSON)
             }
@@ -84,6 +85,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             weatherDataModel.city = json["name"].stringValue
             weatherDataModel.condition = json["weather"][0]["id"].intValue
             weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            weatherDataModel.weatherBackgroundName = weatherDataModel.updateWeatherBackground(condition: weatherDataModel.condition)
+            weatherDataModel.weatherCondition = weatherDataModel.updateWeatherCondition(condition: weatherDataModel.condition)
             
             updateUIWithWeatherData()
         }
@@ -102,8 +105,11 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     //Write the updateUIWithWeatherData method here:
     func updateUIWithWeatherData(){
         cityLabel.text = weatherDataModel.city
-        temperatureLabel.text = "\(weatherDataModel.temperature)˚"
+        temperatureLabel.text = "\(weatherDataModel.temperature)˚C"
+        conditionLabel.text = weatherDataModel.weatherCondition
         weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+        
+        weatherBackground.image = UIImage(named: weatherDataModel.weatherBackgroundName)
     }
     
     
@@ -148,11 +154,21 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     
     //Write the userEnteredANewCityName Delegate method here:
-    
+    func userEnteredANewCityName(city: String) {
+        
+        let params : [String: String] = ["q": city, "appid": APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
 
     
     //Write the PrepareForSegue Method here
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "changeCityName" {
+            let destinationVC = segue.destination as! ChangeCityViewController
+            
+            destinationVC.delegate = self
+        }
+    }
     
     
     
